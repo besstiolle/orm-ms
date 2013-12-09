11632eabfc497c99f260a7273cb9d0688b606af4
besstiolle <contact@furie.be>
2013-12-09 23:48:47 +0100

    adding a cleaning task into the upgrade process of OrmSkeleton

--- src/OrmSkeleton/method.upgrade.php
+++ src/OrmSkeleton/method.upgrade.php
@@ -2,6 +2,12 @@
 
 if (!function_exists("cmsms")) exit;
 
+//Drop all the tables and recreate them
+$entities = MyAutoload::getAllInstances($this->GetName(), $this->getName());
+foreach($entities as $anEntity) {
+	Core::dropTable($anEntity);
+	Core::createTable($anEntity);
+}
 
 // put mention into the admin log
 $this->Audit( 0,  $this->Lang('friendlyname'),  $this->Lang('upgraded', $this->GetVersion()));
