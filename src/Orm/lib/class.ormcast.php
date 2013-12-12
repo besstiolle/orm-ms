8372e05ff48151d2d2f3e8a1365efb1d92801c1a
besstiolle <contact@furie.be>
2013-12-12 23:06:24 +0100

    Reorder Cast values + add documentation

--- src/Orm/lib/class.ormcast.php
+++ src/Orm/lib/class.ormcast.php
@@ -12,12 +12,18 @@
  * Class Defines the type of the field, in entity but also in database
  * 
  *  OrmCAST::$STRING a simple string
+ *  OrmCAST::$BUFFER a field with no limit of size (except the mysql natural limit)
+ *
  *  OrmCAST::$INTEGER an integer
+ *  OrmCAST::$NUMERIC a field for a real number (eg : with coma)
+ *  OrmCAST::$DOUBLE a field for big number
+ * 
  *  OrmCAST::$DATE a field date
  *  OrmCAST::$TIME a field time
  *  OrmCAST::$TS a field timestamp
- *  OrmCAST::$BUFFER a field with no limit of size (except the mysql natural limit)
- *  OrmCAST::$NUMERIC a field for a real number (eg : with coma)
+ *  OrmCAST::$DATETIME a field dateTime
+ *  OrmCAST::$DATETIME a field UUID
+ *
  *  OrmCAST::$NONE a Transiant field : won't be persist into database
  *
  * @since 0.0.1
@@ -27,15 +33,18 @@
 class OrmCAST
 {
 	public static $STRING = 0;
-	public static $INTEGER = 1;
-	public static $DATE = 2;
-	public static $BUFFER = 3;
-	public static $NUMERIC = 4;
-	public static $TIME = 5;
-	public static $TS = 6;
-	public static $DATETIME = 7;
-	public static $UUID = 8;
-	public static $DOUBLE = 9;
+	public static $BUFFER = 1;
+	
+	public static $INTEGER = 2;
+	public static $NUMERIC = 3;
+	public static $DOUBLE = 4;
+	
+	public static $DATE = 5;
+	public static $TIME = 6;
+	public static $TS = 7;
+	public static $DATETIME = 8;
+	
+	public static $UUID = 9;
 		  
 	public static $NONE = 99;
 }
