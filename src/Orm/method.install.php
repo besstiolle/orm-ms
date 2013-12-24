4ccb61cdff571069fdbc5cf7145fe2576cef4525
besstiolle <contact@furie.be>
2013-12-24 17:35:46 +0100

    try to fix load class during installation.

--- src/Orm/method.install.php
+++ src/Orm/method.install.php
@@ -2,6 +2,13 @@
 
 if (!function_exists("cmsms")) exit;
 
+$config = cmsms()->GetConfig();
+if( !class_exists(OrmTrace)) {
+	require_once($config['root_path'].'/modules/Orm/lib/class.ormtrace.php');
+}
+if( !class_exists(OrmCache)) {
+	require_once($config['root_path'].'/modules/Orm/lib/class.ormcache.php');
+}
 
 $this->SetPreference('loglevel', OrmTrace::$INFO);
 $this->SetPreference('cacheType', OrmCache::$NONE);
