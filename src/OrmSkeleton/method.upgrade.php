bcee937322690b7c1a18e066e48070e60830048c
Kevin Danezis <kdanezis@sii.fr>
2014-10-10 11:48:12 +0200

    fix #84 OrmSkeleton upgrade method to be commented

--- src/OrmSkeleton/method.upgrade.php
+++ src/OrmSkeleton/method.upgrade.php
@@ -5,6 +5,7 @@ if (!function_exists("cmsms")) exit;
 //Drop all the tables and recreate them
 $entities = MyAutoload::getAllInstances($this->GetName(), $this->getName());
 foreach($entities as $anEntity) {
+	// NEVER DO LIKE THIS IN YOUR MODULES, SEE HOW TO HANDLE AN UPGRADE, USING SWITCH
 	OrmCore::dropTable($anEntity);
 	OrmCore::createTable($anEntity);
 }
